6956ee34be6b801940b9277654ddb28b8d04804a
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-03-11 23:22:15 +0100

    Add Particles and change background.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Title from './components/title'
 import Welcome from './components/welcome'
 import { Grid, AppBar, Box } from '@material-ui/core';
 import RavensuAppBar from './components/RavensuAppBar'
+import Particles from 'react-particles-js'
 
 function App() {
   return (
     <>
+
     <div className = "App">
       <Box p ={4} color = "transparent">
       <RavensuAppBar/>
       </Box>
-
+      <Particles params={{
+        "particles":{
+          "number":{
+            "value": 80
+          },
+        }
+      }}/>
     </div>
     </>
   );

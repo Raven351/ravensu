f621b555f4c2a94e49a29e3a60cfc62ce1bbc33c
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-03-15 19:52:48 +0100

    Add AboutCard.js

---
+++ src/components/Cards/AboutCard.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import {Box, Paper, makeStyles, Typography, CardHeader, Avatar, CardContent, Grid, Divider} from '@material-ui/core'
+import Card from '@material-ui/core/Card'
+import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
+import ProfilePicture from "../../img/profilepicture.jpg"
+import HomeIcon from '@material-ui/icons/Home';
+import SchoolIcon from '@material-ui/icons/School';
+import SmsIcon from '@material-ui/icons/Sms';
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: 20,
+        color: "#000000",
+        backgroundColor: "#ebebeb",
+        opacity: "86%",
+        boxShadow: 10,
+    },
+
+    card: {
+        maxWidth: 350,
+        minWidth: 400,
+        //backgroundColor: "transparent"
+    },
+
+    avatar:{
+        backgroundColor: "#000000",
+        width: theme.spacing(20),
+        height: theme.spacing(20)
+    },
+
+    icon:{
+        fontSize: 80,
+    },
+
+    dividerColor:{
+        backgroundColor: "#000000",
+        color: "#000000",
+        height: 1
+    }
+
+}));
+
+function AvatarContent(){
+    const classes = useStyles()
+    return(
+        <Grid 
+        container
+        display = "flex"
+        direction = "column"
+        justify = "center"
+        alignItems = "center"
+        >
+            <Grid item style = {{padding: 2}} >
+                <Avatar className = {classes.avatar} src={ProfilePicture} alt="Bartosz Baum" style = {{alignSelf: "center"}}/>
+            </Grid>
+            
+            <Divider variant = "fullWidth" classes = {{root: classes.dividerColor}} flexItem = "true" />
+
+            <Grid item style = {{marginTop: 10}}>
+                <Typography
+                variant = "h5"
+                component = "h1"
+                >
+                Bartosz Baum<br/>
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+}
+
+function CardContentGrid(props){
+    return(
+        <Grid
+        display = "flex"
+        direction = "column"
+        justify = "flex-start"
+        alignItems = "flex-start"
+        >
+            {props.children}
+        </Grid>
+    );
+}
+
+function CardContentGridDescriptionElement(props){
+    return(
+        <Grid 
+        container
+        display = "flex"
+        direction = "column"
+        justify = "center"
+        alignItems = "center"
+        >
+            {props.children}
+        </Grid>
+    );
+}
+
+export default function AboutCard(){
+    const classes = useStyles();
+    return ( 
+        <Card className = {classes.card}>
+            <CardContent style={{alignContent: "center"}}>
+                <AvatarContent/>
+                <CardContentGrid>            
+                    <CardContentGridDescriptionElement>
+                        <Grid item style = {{marginTop: 20}}>
+                            <HomeIcon/> 
+                        </Grid>
+                        <Grid item >
+                            <Typography
+                            variant = "body1"
+                            align = "center"
+                            style ={{color: "#000000"}}                    
+                            >
+                            Gdańsk, Poland
+                            </Typography>
+                        </Grid>
+                    </CardContentGridDescriptionElement>
+                    <CardContentGridDescriptionElement>
+                        <Grid item style = {{marginTop: 20}}>
+                            <SchoolIcon/> 
+                        </Grid>
+                        <Grid item>
+                            <Typography
+                            variant = "body1"
+                            align = "center"                     
+                            >
+                            IT Engineer Degree<br/>
+                            </Typography>
+                        </Grid>
+                    </CardContentGridDescriptionElement>
+                    <CardContentGridDescriptionElement >
+                        <Grid item style = {{marginTop: 20}} >
+                            <SmsIcon/> 
+                        </Grid>
+                        <Grid item >
+                            <Typography
+                            variant = "body1"
+                            align = "center"                     
+                            >
+                                Polish - Native<br/>
+                                English - Advanced (C1)<br/>
+                                Japanese - Basic (N4)<br/>
+
+                            </Typography>
+                        </Grid>
+                    </CardContentGridDescriptionElement>
+                </CardContentGrid>
+
+            </CardContent>
+        </Card> 
+    ); 
+}
--- src/components/Tabs/About.js
+++ src/components/Tabs/About.js
@@ -6,6 +6,7 @@ import ProfilePicture from "../../img/profilepicture.jpg"
 import HomeIcon from '@material-ui/icons/Home';
 import SchoolIcon from '@material-ui/icons/School';
 import SmsIcon from '@material-ui/icons/Sms';
+import AboutCard from '../Cards/AboutCard'
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -42,104 +43,6 @@ const useStyles = makeStyles(theme => ({
 export default function About(){
     const classes = useStyles();
         return(
-            <Card className = {classes.card}>
-                <CardContent style={{alignContent: "center"}}>
-                    <Grid 
-                    container
-                    display = "flex"
-                    direction = "column"
-                    justify = "center"
-                    alignItems = "center"
-                    >
-                        <Grid item style = {{padding: 2}} >
-                            <Avatar className = {classes.avatar} src={ProfilePicture} alt="Bartosz Baum" style = {{alignSelf: "center"}}/>
-                        </Grid>
-                        
-                        <Divider variant = "fullWidth" classes = {{root: classes.dividerColor}} flexItem = "true" />
-
-                        <Grid item style = {{marginTop: 20}}>
-                            <Typography
-                            variant = "h5"
-                            component = "h1"
-                            >
-                            Bartosz Baum<br/>
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid                    
-                    container
-                    display = "flex"
-                    direction = "column"
-                    justify = "flex-start"
-                    alignItems = "flex-start"
-                    >            
-                        <Grid 
-                        container
-                        display = "flex"
-                        direction = "column"
-                        justify = "center"
-                        alignItems = "center"
-                        >
-                            <Grid item >
-                                <HomeIcon/> 
-                            </Grid>
-                            <Grid item >
-                                <Typography
-                                variant = "body1"
-                                align = "center"                      
-                                >
-                                Gdańsk, Poland
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid 
-                        container
-                        display = "flex"
-                        direction = "column"
-                        justify = "center"
-                        alignItems = "center"
-                        >
-                            <Grid item >
-                                <SchoolIcon/> 
-                            </Grid>
-                            <Grid item >
-                                <Typography
-                                variant = "body1"
-                                align = "center"                     
-                                >
-                                IT Engineer Degree<br/>
-                                WSB University in Gdańsk <br/>
-                                Information Technology <br/>
-                                Programming Specialization<br/>
-
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid 
-                        container
-                        display = "flex"
-                        direction = "column"
-                        justify = "center"
-                        alignItems = "center"
-                        >
-                            <Grid item style = {{marginTop: 20}} >
-                                <SmsIcon/> 
-                            </Grid>
-                            <Grid item >
-                                <Typography
-                                variant = "body1"
-                                align = "center"                     
-                                >
-                                    Polish - Native<br/>
-                                    English - Advanced (C1)<br/>
-                                    Japanese - Basic (N4)<br/>
-
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-
-                </CardContent>
-            </Card>  
+            <AboutCard/>
         )
     }

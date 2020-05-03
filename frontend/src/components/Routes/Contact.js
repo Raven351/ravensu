4b66b2740dd150b2488f66140a1a7afe9c8e3a9d
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-05-03 16:57:47 +0200

    Add email send in progress loading screen and success notification.

--- frontend/src/components/Routes/Contact.js
+++ frontend/src/components/Routes/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
-import {Container, TextField, createMuiTheme, ThemeProvider, Button} from '@material-ui/core'
+import {Container, TextField, createMuiTheme, ThemeProvider, Button, Dialog, DialogTitle, CircularProgress, withStyles, Avatar} from '@material-ui/core'
 import SendIcon from '@material-ui/icons/Send';
+import CheckIcon from '@material-ui/icons/Check';
+import CancelIcon from '@material-ui/icons/Cancel';
 import {blue} from '@material-ui/core/colors'
 
 const useStyles = makeStyles (theme => ({
@@ -9,6 +11,9 @@ const useStyles = makeStyles (theme => ({
         backgroundColor: "rgba(255,255,255,0.9)",
         borderRadius: 4,
         
+    },
+    emailSendingDialogRoot:{
+        margin: 20
     },
 }));
 
@@ -18,12 +23,57 @@ const textFieldTheme = createMuiTheme({
     }
 })
 
+const ColoredCircularProgress = withStyles({
+    root: {
+        color: '#ffffff'
+    }
+})(CircularProgress);
+
+function EmailSendingDialog(props){
+    const classes = useStyles();
+    const {isOpen, onClose, showSendingEmailProgress, showEmailSendSuccedIcon, showEmailSendFailIcon} = props;
+    return(
+        <div className = {classes.emailSendingDialogRoot}>
+            <Dialog open = {isOpen} PaperProps = {{style: {backgroundColor: "transparent", boxShadow: "none", padding: 50}}}>
+                {showSendingEmailProgress && <ColoredCircularProgress size = {100} />}
+                {showEmailSendSuccedIcon && <Avatar style = {{height: 100, width: 100, backgroundColor: "#59e387"}}><CheckIcon style = {{fontSize: "48px"}} /></Avatar>}
+                {showEmailSendFailIcon && <Avatar style = {{height: 100, width: 100, backgroundColor: "#59e387"}}><CancelIcon style = {{fontSize: "48px"}} /></Avatar>}
+            </Dialog>
+        </div>
+
+    );
+}
+
+function EmailSentStatusDialog(props){
+    const classes = useStyles();
+    const {isOpen, onClose, dialogMessage} = props;
+    return(
+        <div>
+            <Dialog>
+                <DialogTitle>{"Error"}</DialogTitle>
+            </Dialog>
+        </div>
+    )
+}
+
 function Contact(props){
     const classes = useStyles();
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [subject, setSubject] = React.useState('');
-    const [message, setMessage] = React.useState('');
+    const fetchEmailTimoutTimer = React.useRef();
+    const emailSendingNotificationTimer = React.useRef();
+    const [name, setName] = React.useState('Temp Name');
+    const [email, setEmail] = React.useState('Temp email@email.com');
+    const [subject, setSubject] = React.useState('Temp Subject');
+    const [message, setMessage] = React.useState('Temp message some message');
+    const [isEmailSendingDialogOpen, setEmailSendingDialogOpen] = React.useState(false);
+    const [showSendingEmailProgress, setShowSendingEmailProgress] = React.useState(false);
+    const [showEmailSendSuccedIcon, setShowEmailSendSuccedIcon] = React.useState(false);
+    const [showEmailSendFailIcon, setShowEmailSendFailIcon] = React.useState(false);
+    const [emailSentStatusDialog, setEmailSentStatusDialog] = React.useState(false);
+    React.useEffect(() => {
+        return () => {
+          clearTimeout(emailSendingNotificationTimer.current);
+        };
+      }, []);
     let mailData = {
         name: name,
         email: email,
@@ -48,9 +98,16 @@ function Contact(props){
     const onMessageChange = (e) =>{
         setMessage(e.target.value);
     }
-
+    const handleEmailSendingDialogClose = () =>{
+        setEmailSendingDialogOpen(false);
+    }
     const handleSubmit = (e) =>{
         e.preventDefault();
+        setEmailSendingDialogOpen(true);
+        setShowSendingEmailProgress(true);
+        fetchEmailTimoutTimer.current = setTimeout(() => {
+            
+        }, 10000);
         fetch('http://localhost:8001/send',{
             method: "POST",
             body: JSON.stringify(mailData),
@@ -62,16 +119,28 @@ function Contact(props){
             response => (response.json())
         ).then((response) =>{
             if (response.status === 'success'){
-                alert("Message sent");
-                resetForm();
+                setShowSendingEmailProgress(false);
+                setShowEmailSendSuccedIcon(true);
+                emailSendingNotificationTimer.current = setTimeout(()=>{
+                    setShowEmailSendSuccedIcon(false);
+                    setEmailSendingDialogOpen(false);
+                }, 2000);
+                //resetForm();
             }
             else if(response.status === 'fail'){
-                alert("Message failed to sent")
+                setShowSendingEmailProgress(false);
+                setShowEmailSendFailIcon(true);
+                emailSendingNotificationTimer.current = setTimeout(()=>{
+                    setShowEmailSendFailIcon(false);
+                    setEmailSendingDialogOpen(false);
+                }, 1000);
+                //details dialog
             }
         })
     }
 
     return(
+        <>
         <form onSubmit = {handleSubmit.bind(this)}>
             <Container>
                 <ThemeProvider theme = {textFieldTheme}>
@@ -92,6 +161,14 @@ function Contact(props){
                 </Button>
             </Container>
         </form>
+        <EmailSendingDialog 
+        isOpen = {isEmailSendingDialogOpen} 
+        onClose = {handleEmailSendingDialogClose}
+        showSendingEmailProgress = {showSendingEmailProgress}
+        showEmailSendSuccedIcon = {showEmailSendSuccedIcon}
+        showEmailSendFailIcon = {showEmailSendFailIcon}
+        />
+        </>
     );
 }
 

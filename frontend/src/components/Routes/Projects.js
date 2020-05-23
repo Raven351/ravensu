5366a5de08c987644cf402f99055902e32dacfa8
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-05-23 16:12:43 +0200

    Add displayProjectCards to Projects.js

--- frontend/src/components/Routes/Projects.js
+++ frontend/src/components/Routes/Projects.js
@@ -1,26 +1,28 @@
 import React, {useEffect} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
-import {Grid, Grow, Paper, SwipeableDrawer} from '@material-ui/core'
+import {Grid, Grow, Paper, SwipeableDrawer, Typography} from '@material-ui/core'
 import { ProjectCard } from '../Cards/ProjectCard';
 import { withRouter, Route, useRouteMatch, Switch, Link, Redirect, useLocation, useHistory, useParams } from 'react-router-dom';
 import ProjectDetailsCard from '../Cards/ProjectDetailsCard'
 import ProjectDetails from '../ProjectDetails'
 import Project from '../Project'
+import TextLang from '../TextLang'
+import ProjectsList from '../Constant/ProjectsList'
 
 export const useStyles = makeStyles( theme => ({
-    root: {
-        marginTop: "100px",
+    rootGrid: {
+        marginTop: "50px",
         [theme.breakpoints.down("sm")]:{
             width: "100vw"
         },
-        width: "99vw"
+        width: "100vw"
         
     },
 
     containerCard:{
         width: 350,
         [theme.breakpoints.down("sm")]:{
-            marginLeft: "40px",
+            marginLeft: "20px",
         },
     },
 
@@ -43,23 +45,36 @@ function Projects(){
     }
     const [projectDetailsDrawerState, setProjectDetailsDrawerState] = React.useState(false);
     const [selectedProjectState, setSelectedProjectState] = React.useState(0);
+    const displayProjectCards = ProjectsList.map(({key})=>(
+        <Grid item className = {classes.containerCard}><ProjectCard projectId = {key} onClick = {() => openProjectDetailsDrawer(key)}/></Grid>
+    ));
     return(
         <>
         <Grid
             container
-            className = {classes.root}
+            className = {classes.rootGrid}
             direction = "row"
             alignItems = "center"
             justify = "center"
             spacing = {5}
         >
-            <Grid item className = {classes.containerCard}><ProjectCard projectId = {0} onClick = {() => openProjectDetailsDrawer(0)}/></Grid>
-            <Grid item className = {classes.containerCard}><ProjectCard projectId = {1} onClick = {() => openProjectDetailsDrawer(1)}/></Grid>
+            <Grid item xs = {12} style = {{marginBottom: "20px"}}>
+                <Typography
+                variant = "h4"
+                component = "h1"
+                align = "center"
+                style = {{color: "#ffffff", fontFamily: "Baloo Thambi 2"}}
+                >
+                    <TextLang textId = "projectHeader"/>
+                </Typography>
+            </Grid>
+            {displayProjectCards}
         </Grid>
         <SwipeableDrawer open = {projectDetailsDrawerState} onOpen = {toggleProjectDetailsDrawer(true)} onClose={toggleProjectDetailsDrawer(false)} anchor = "right" variant = "temporary" >
             <ProjectDetails projectId = {selectedProjectState}/>
         </SwipeableDrawer>
         </>
+
     );
 }
 
